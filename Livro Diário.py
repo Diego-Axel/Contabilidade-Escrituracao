763d86961673b41bcf2947b99b0da5359a33656c
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2023-12-05 16:20:40 -0300

    Update Livro Diário.py
    Upload de outro arquivo que eu havia criado

---
+++ Livro Diário.py
@@ -0,0 +1,55 @@
+# PROGRAMA PARA SE UTILIZAR A CONTABILIDADE BÁSICA(DIÁRIO, RAZONETE, BALANECETE E BALANÇO PATRIMONIAL)
+
+# PROJETO APENAS PARA USO PESSOAL COMO TREINAMENTO (NÃO SEI SE LEVAREI ISSO ADIANTE :D )
+
+print("-----------------------------------------------")
+print("|                LIVRO DIÁRIO                 |" )
+print("-----------------------------------------------")
+
+# Bom, a começar a explicar o que seria um diário para o mundo da contabilidade. Basicamente seria como se a gente estivesse fazendo um resgistro de toda a movimentação que ocorreu na empresa ou comércio da entidade(Pessoa Física ou Pessoa Jurídica), essa movimentação se da por: Compra de mercadoria, fluxo do caixa(Entrada e saída de U$$), recebimento de dinheiro de vendas, enfim, tudo aquilo que possa alterar o patrimônio  da entide.
+
+print("-----------------------------------------------")
+empresa = input("Qual o nome da sua empresa(LTda)? ")
+print("-----------------------------------------------")
+print(f" Empresa {empresa}, vamos fazer o levantamento do seu livro diário. ")
+print("-----------------------------------------------")
+
+# Após a pessoa ter dado o nome de sua empresa, agora chegou a hora de escolher o número de suas operações. É simples, a contabilidade tem uma linguagem única que utiliza o Débito(D) e o Crédito(C), mas esse débito e crédito não são os mesmos que são usados no nosso cotidiano, entenda aqui na contabulidade como débito=algo bom e crédito=oposto.
+
+print("Quantas operações deseja realizar (Máximo de 10 (BETA))? ") #Ainda desenvolvendo...
+numero_op = int(input(""))
+#--------------------------------------------------------------
+
+if numero_op == 1:
+    print("-----------------------------------------------")
+    #........................
+   
+# CORPO ---> BASE
+#
+# Operação(x) data xx/xx/xxx
+#
+# D(débito) caixa - U$$ XX.XXXX,XX
+# C(Crédito) Mercadoria - U$$ XX.XXXX,XX 
+#
+# Valores e contas superficiais só de base
+#
+# Histórico - xxxxxx.... 
+
+print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+print(f"")
+print(f"")
+print(f"")
+print(f"")
+print("-----------------------------------------------")
+print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+print(f"")
+print(f"")
+print(f"")
+print(f"")
+print("-----------------------------------------------")
+print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+print(f"")
+print(f"")
+print(f"")
+print(f"")
+print("-----------------------------------------------")
