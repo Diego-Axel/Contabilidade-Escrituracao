feee0fb654d644ca6dad15286bcebe0a995d4482
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2023-12-05 20:10:17 -0300

    Update Explicação.py
    Comecei a dar uma leve explicação de como funciona a escrituração e o que ela vai conter neste programa.

---
+++ Explicação.py
@@ -0,0 +1,24 @@
+# Uma breve explicação de o que seria escrituração contábil e como eu planejo fazer ela "rodar" em PYTHON
+
+# Basicamente a escrituração contábil é meio que você pegar os fatos contábeis, que é tudo aquilo que altera o patrimônio da empresa, e esses fatos podem ser a entrada e saída de dinheiro no caixa da empresa, vendas de mercadorias, estoques, enfim, tudo que estiver envolvido em alteração do patrimônio.
+
+# A escrituração ela é divida em algumas parte ->
+# 1º Livro Diário, onde nele vai ter o Débito(D) e o Crédito(C) que a soma dos débitos e a soma dos créditos, se houver mais de uma daram o mesmo resultado.
+#--------------------------------------------------------------------------------------------------------------------------------------
+# 2º Livro Razão(RAZONETE) que é representado da seguinte forma:
+#
+# ->
+#    
+# (nome da conta)
+#  _____________            
+#       |
+#       |
+#       |
+#       |
+#
+# São representados em formato de "T" onde em cima desse "T" fica o nome da conta(Caixa, banco, estoques, fornecedor, clientes,...) do lado esquerdo, os débitos e o lado esquerdo, é a parte dos créditos. É por meio do Razonete que são feitos os registros individuais por conta.
+#--------------------------------------------------------------------------------------------------------------------------------------
+# 3º É o Balanço Patrimonial que é um relatório que demonstra de maneira clara e precisa a situação financeira de uma empresa. Para isso, são considerados todos os ativos e passivos de um negócio, ou seja, seus bens, dívidas e lucros.
+#
+#
+#
