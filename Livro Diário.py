7dfe29eea8516516efb926357e607305f4e993d4
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2023-12-06 17:02:34 -0300

    Update do livro do Diário
    Adicionando as condições ao programa

--- Livro Diário.py
+++ Livro Diário.py
@@ -11,7 +11,7 @@ print("-----------------------------------------------")
 print("-----------------------------------------------")
 empresa = input("Qual o nome da sua empresa(LTda)? ")
 print("-----------------------------------------------")
-print(f" Empresa {empresa}, vamos fazer o levantamento do seu livro diário. ")
+print(f"Empresa {empresa}, vamos fazer o levantamento do seu livro diário. ")
 print("-----------------------------------------------")
 
 # Após a pessoa ter dado o nome de sua empresa, agora chegou a hora de escolher o número de suas operações. É simples, a contabilidade tem uma linguagem única que utiliza o Débito(D) e o Crédito(C), mas esse débito e crédito não são os mesmos que são usados no nosso cotidiano, entenda aqui na contabulidade como débito=algo bom e crédito=oposto.
@@ -23,8 +23,8 @@ print("-----------------------------------------------")
 print("Quantas contas você deseja adicionar? ")
 numero_conta = input("")
 
-if numero_conta == 2:
-    print("Se você escolheu 2, então é porque você tem um (D) e um (C) -> a sua primeira conta será atribuida o valor ao débito e a segunda ao crédito.")
+if numero_op == 1 and numero_conta == 2:
+    print(f"Você escolheu {numero_op} operações. Número de contas você escolheu {numero_conta}, então é porque você tem um (D) e um (C) -> a sua primeira conta será atribuida o valor ao débito e a segunda ao crédito.")
     print("-----------------------------------------------")
 
     conta1 = input("Digite o nome da sua conta (caixa, banco, mercadoria...)")
@@ -36,7 +36,7 @@ if numero_conta == 2:
     print("-----------------------------------------------")
 
     d1 = conta1 
-    c1 = conta2
+    c1 = conta2       
 
     print(f"Você tem a conta {conta1} com um valor de R${valor_c1} ")
     print(f"Você tem a conta {conta2} com um valor de R${valor_c2} ")
@@ -46,6 +46,37 @@ if numero_conta == 2:
     print("-->")
     print(f"D - {conta1}   R$ {valor_c1}")
     print(f"C - {conta2}   R$ {valor_c2}")
+    print("-----------------------------------------------")    
+elif numero_op == 2 and numero_conta == 2:
+    print(f"Você escolheu {numero_op} operações. Número de contas você escolheu {numero_conta}, então é porque você tem um (D) e um (C) -> a sua primeira conta será atribuida o valor ao débito e a segunda ao crédito.")
+    print("-----------------------------------------------")
+
+    conta1 = input("Digite o nome da sua conta (caixa, banco, mercadoria...)")
+    conta2 = input("Digite o nome da sua conta (caixa, banco, mercadoria...)")
+    print("-----------------------------------------------")
+
+    valor_c1 = float(input(f"Qual o valor que será atribuido a conta {conta1}? "))
+    valor_c2 = float(input(f"Qual o valor que será atribuido a conta {conta2}? "))
+    print("-----------------------------------------------")
+
+    d1 = conta1 
+    c1 = conta2       
+
+    print(f"Você tem a conta {conta1} com um valor de R${valor_c1} ")
+    print(f"Você tem a conta {conta2} com um valor de R${valor_c2} ")
+    print("-----------------------------------------------")
+
+    print("O lançamento ficou:")
+    print("-->")
+    print(f"D - {conta1}   R$ {valor_c1}")
+    print(f"C - {conta2}   R$ {valor_c2}")
+    print("-----------------------------------------------")
+
+    conta3 = input("Digite o nome da sua conta (caixa, banco, mercadoria...)")
+    conta3 = input("Digite o nome da sua conta (caixa, banco, mercadoria...)")
+    
+    print("-----------------------------------------------")
+    
 
   
   
@@ -63,23 +94,23 @@ if numero_conta == 2:
 #
 # Histórico - xxxxxx.... 
 
-print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
-print(f"")
-print(f"")
-print(f"")
-print(f"")
-print("-----------------------------------------------")
+# print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+# print(f"")
+# print(f"")
+# print(f"")
+# print(f"")
+# print("-----------------------------------------------")
 
-print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
-print(f"")
-print(f"")
-print(f"")
-print(f"")
-print("-----------------------------------------------")
+# print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+# print(f"")
+# print(f"")
+# print(f"")
+# print(f"")
+# print("-----------------------------------------------")
 
-print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
-print(f"")
-print(f"")
-print(f"")
-print(f"")
-print("-----------------------------------------------")
+# print(f"Operação número {numero_op} data xx/xx/xxx") # cadastrar variaveis 
+# print(f"")
+# print(f"")
+# print(f"")
+# print(f"")
+# print("-----------------------------------------------")
