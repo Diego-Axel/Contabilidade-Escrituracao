c7ebc5000f53035c5721037eb23894d7e39abfb3
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2023-12-13 23:13:16 -0300

    Adicionado Condições P.2

--- Livro Diário.py
+++ Livro Diário.py
@@ -6,6 +6,20 @@ print("-----------------------------------------------")
 print("|                LIVRO DIÁRIO                 |")
 print("-----------------------------------------------")
 
+print("Para começar, digite o nome de sua empresa(Ltda):")
+nome_empresa = input("")
+print("-----------------------------------------------")
+print(f"Empresa {nome_empresa}, está prestes a começar o seu livro diário.")
+print("-----------------------------------------------")
+numero_lancamentos  = int(input("Digite quantos lançamentos você pretende ralizar (mínimo 2): "))
+print("-----------------------------------------------")
+
+if numero_lancamentos == 2:
+    print(f"Número de Lançamentos escolhidos: {numero_lancamentos}")
+else:
+    print("")
+
+
 
 
     
