4779f9a7941d79c09939f895c0e833d0a0bddc5d
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2023-12-13 22:49:41 -0300

    Update Livro Diário.py

--- Livro Diário.py
+++ Livro Diário.py
@@ -7,20 +7,5 @@ print("|                LIVRO DIÁRIO                 |")
 print("-----------------------------------------------")
 
 
-print("-----------------------------------------------")
-empresa = input("Qual o nome da sua empresa(LTda)? ")
-print("-----------------------------------------------")
-print(f"Empresa {empresa}, vamos fazer o levantamento do seu livro diário. ")
-print("-----------------------------------------------")
-
-# Após a pessoa ter dado o nome de sua empresa, agora chegou a hora de escolher o número de suas operações. É simples, a contabilidade tem uma linguagem única que utiliza o Débito(D) e o Crédito(C), mas esse débito e crédito não são os mesmos que são usados no nosso cotidiano, entenda aqui na contabulidade como débito=algo bom e crédito=oposto.
-
-print("Quantas operações deseja realizar (Máximo de 10 (BETA))? ") #Ainda desenvolvendo...
-numero_op = int(input(" "))
-#--------------------------------------------------------------
-print("-----------------------------------------------")
-print("Quantas contas você deseja adicionar? ")
-numero_conta = input("")
-print("-----------------------------------------------")
 
     
