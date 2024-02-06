b4ccb36d4b68e28844b7c2cabff65a5ff3db9e22
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-06 18:48:06 -0300

    Create Explicacao.py

---
+++ Explicacao.py
@@ -0,0 +1,58 @@
+# Projeto simples usando conhecimentos adquirioos em Ciências Contabéis (BÁSICA I and II)
+
+# Uma breve explicação de o que seria escrituração contábil e como eu planejo fazer ela "rodar" em PYTHON
+
+# Basicamente a escrituração contábil é meio que você pegar os fatos contábeis, que é tudo aquilo que altera o patrimônio da empresa, e esses fatos podem ser a entrada e saída de dinheiro no caixa da empresa, vendas de mercadorias, estoques, enfim, tudo que estiver envolvido em alteração do patrimônio.
+
+# A escrituração ela é divida em algumas parte ->
+# 1º Livro Diário, onde nele vai ter o Débito(D) e o Crédito(C) que a soma dos débitos e a soma dos créditos, se houver mais de uma daram o mesmo resultado. Para deixar bem claro, no livro Diário, pelo menos no momento deste commit, ele está na fase de aperfeiçoamento. Eu ainda deixarei ele mais automatizado no decorrer que eu for ganahando experiência.
+
+# Assim que planejo deixar ao final de cada lançamento:
+# print("Data do lançamento: xx/xx/xxxx")
+# print("D/C - (nome da conta) - R$ xx.xxx,xx -> (valor da conta)")
+# print("D/C - (nome da conta) - R$ xx.xxx,xx -> (valor da conta)")
+# Pode haver mais de um DÉBITO e mais de um CRÉDITO:
+# print("Histórico referente a está operação: xxxxxxxxxxxx")
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# 2º Livro Razão(RAZONETE) que é representado da seguinte forma:
+#
+# ->
+#    
+# (nome da conta)
+#  _____________            
+#       |
+#       |
+#       |
+#       |
+#
+# São representados em formato de "T" onde em cima desse "T" fica o nome da conta(Caixa, banco, estoques, fornecedor, clientes,...) do lado esquerdo, os débitos e o lado esquerdo, é a parte dos créditos. É por meio do Razonete que são feitos os registros individuais por conta.
+#
+# Ficaria de uma certa forma desse jeito:
+#
+# print("_DÉBITOS__________NOME_DA_CONTA__________CRÉDITOS_")
+# print("                        |                         ")
+# print("                        |                         ")
+# print("                        |                         ")
+# print("                        |                         ")
+# print("                        |                         ")
+# print("__________________________________________________")
+# print("                        |                         ")
+#
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+# o 3º e OPCIONAL é o Balancete, que consiste e, basicamente tirar uma "prova real" antes do balamço patrimônial , ele dando certo, ou seja, --> ATIVOS == PASSIVOS <-- Tudo ok. 
+  
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+# 4º É o Balanço Patrimonial que é um relatório que demonstra de maneira clara e precisa a situação financeira de uma empresa. Para isso, são considerados todos os ativos e passivos de um negócio, ou seja, seus bens, dívidas e lucros.
+#
+#
+# Bom como eu estou falando de contabilidade básica, o Balanço Patrimonial(BP) ele será também feito assim como é visto na faculdade de contabilidade bem ao básico mesmo --->
+#
+# Com o lado esquerdo tendo seus ativos, as suas contas com o seu saldo e o lado direito o passivo que assim oomo o ativo tem suas contas e o seus saldos e logo abaixo do passivo no mesmo lado temos o patrimônio Líquido com seu capital(R$)
+#
+# E ao final de cada um dos dois lados vamos ter o total, que na contabilidade, ATIVO = PASSIVO + PATRIMÔNIO LÍQUIDO.
+#
+# O balanço patrimonial é feito com base em um período de tempo, uma vez ao ano. Portanto, é preciso definir quais serão os meses analisados, o que pode variar conforme as necessidades do negócio. 
