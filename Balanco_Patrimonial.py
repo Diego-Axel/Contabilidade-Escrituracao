019fa2e868a3dafb26e6a0dc9d0e88149b642e16
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-06 18:46:56 -0300

    Create Balanco_Patrimonial.py

---
+++ Balanco_Patrimonial.py
@@ -0,0 +1,49 @@
+# Projeto simples usando conhecimentos adquirioos em Ciências Contabéis (BÁSICA I and II)
+
+print("########     ###    ##          ###    ##    ##  ######   #######  ")
+print("##     ##   ## ##   ##         ## ##   ###   ## ##    ## ##     ## ")
+print("##     ##  ##   ##  ##        ##   ##  ####  ## ##       ##     ## ")
+print("########  ##     ## ##       ##     ## ## ## ## ##       ##     ## ")
+print("##     ## ######### ##       ######### ##  #### ##       ##     ## ")
+print("##     ## ##     ## ##       ##     ## ##   ### ##    ## ##     ## ")
+print("########  ##     ## ######## ##     ## ##    ##  ######   ####### " )
+print("                                                   ###             ")
+print("                                                    ##             ")
+print("                                                                   ")
+#---------------------------------------------------------------------------
+print("                                                         ###                                     ")
+print("                                                        ## ##                                    ")
+print("                                                       ##   ##                                   ")
+print("                                                                                                 ")
+print("########     ###    ######## ########  #### ##     ##  #######  ##    ## ####    ###    ##       ")
+print("##     ##   ## ##      ##    ##     ##  ##  ###   ### ##     ## ###   ##  ##    ## ##   ##       ")
+print("##     ##  ##   ##     ##    ##     ##  ##  #### #### ##     ## ####  ##  ##   ##   ##  ##       ")
+print("########  ##     ##    ##    ########   ##  ## ### ## ##     ## ## ## ##  ##  ##     ## ##       ")
+print("##        #########    ##    ##   ##    ##  ##     ## ##     ## ##  ####  ##  ######### ##       ")
+print("##        ##     ##    ##    ##    ##   ##  ##     ## ##     ## ##   ###  ##  ##     ## ##       ")
+print("##        ##     ##    ##    ##     ## #### ##     ##  #######  ##    ## #### ##     ## ######## ")
+print("                                                                                                 ")
+
+# ASSIM FICARÁ O BALANÇO PATRIMÔNIAL FINALIZADO
+# Breve legenda:
+# De um lado, ATIVOS(DÉBITOS) e do outro PASSIVOS e PATRIMÔNIO LÍQUIDO(CRÉDITOS)
+# ContaX -> se refere as contas que poderam ser, caixa, banco, fornecedor, capital social, enfim.
+# R$ -> O valor referente a cada conta.
+# TOTAL -> A soma dos valores(R$) de todas as contas do ativo devem dar igual a soma dos valores(R$) das contas do (PASSIVO + PATRIMÔNIO LÍQUIDO). 
+
+print("|_____________________________BALANÇO PATRIMÔNIAL___________________________|")
+print("|_____ATIVOS_____R$__________________|____________PASSIVOS_____R$___________|")
+print("| contaX         R$ xx.xxx.xx        |             contaX      R$ xx.xxx.xx |")
+print("| contaX         R$ xx.xxx.xx        |             contaX      R$ xx.xxx.xx |")
+print("| contaX         R$ xx.xxx.xx        |             contaX      R$ xx.xxx.xx |")
+print("| contaX         R$ xx.xxx.xx        |             contaX      R$ xx.xxx.xx |")
+print("| contaX         R$ xx.xxx.xx        |             contaX      R$ xx.xxx.xx |")
+print("|____________________________________|______________________________________|")
+print("|                                    |                                      |")
+print("|____________________________________|____PATRIMÔNIO LÍQUIDO___R$___________|")
+print("|                                    |          contaX         R$ xx.xxx.xx |")
+print("|                                    |          contaX         R$ xx.xxx.xx |")
+print("|                                    |          contaX         R$ xx.xxx.xx |")
+print("|____________________________________|______________________________________|")
+print("| TOTAL = xx.xxx.xx                  | TOTAL = xx.xxx.xx                    |")
+print("|____________________________________|______________________________________|")
