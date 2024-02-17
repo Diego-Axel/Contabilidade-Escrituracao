918845dcf7630c7a6f15bdcc78dc3a8e5af1f836
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-17 14:59:58 -0300

    Update explicacao.py

--- explicacao.py
+++ explicacao.py
@@ -4,3 +4,7 @@
 ######                   DIÊGO AXEL                   ######
 ######                    CAICÓ-RM                    ######
 ############################################################
+
+# Projeto feito com intutio de exercitar o aprendizado em Python, colocando em prática tudo o que eu aprendi até o momento. Esse projeto ira mudar bastante e até mesmo recomeçar muitas vezes para deixa-ló melhor.
+
+# Comecei a fazer esse projeto pessoal desde o ano passado, em dezembro, poucos meses apoós ter iniciado o contato com a programção. Antes, cada passo da escrituração(Diário, Razão, Balancete e Balanço Patrimônial) eram separados, cada um com seu arquvio em PY.
