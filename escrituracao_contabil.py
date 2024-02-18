b1e25d3d27cbdd77acd2328d39c27c13a130b074
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-18 17:42:39 -0300

    Update escrituracao_contabil.py

--- escrituracao_contabil.py
+++ escrituracao_contabil.py
@@ -179,7 +179,22 @@ while True:
                     print("                                                              ")
                     valorC1 = float(input("Escolha o valor(R$) que será atribuida a conta %s nomeada anteriormente."%contaC1))
                     print("                                                              ")
-                    
+                    contaC2 = input("Escolha o nome da sua OUTRA conta de C´REDITO: ")
+                    print("                                                              ")
+                    valorC2 = float(input("Escolha o valor(R$) que será atribuida a conta %s nomeada anteriormente"%contaC2))
+                    print("                                                              ")
+                    historico1 = input("Digite o histórico referente a está operção: ")
+                    print("                                                              ")
+                    print("O Lançamaneto referente a está operação no Livro Diário ficou da seguinte forma:")
+                    print("_________________________Livro Diário_________________________")
+                    print("                                                              ")
+                    print("DATA DA OPERAÇÃO: %02d/%02d/%d"%(dia,mes,ano))
+                    print("D - %s --- R$ - %d"%(contaD1,valorD1))
+                    print("C - %s --- R$ - %d"%(contaC1,valorC1))
+                    print("C - %s --- R$ - %d"%(contaC2,valorC2))
+                    print("HISTÓRICO: %s"%historico1)
+                    print("                                                              ")
+
 
                     
                 
