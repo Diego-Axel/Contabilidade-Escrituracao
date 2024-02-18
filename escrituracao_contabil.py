af7386faf28ba92e07fafcea976e57f89be498ff
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-18 02:12:23 -0300

    Update escrituracao_contabil.py

--- escrituracao_contabil.py
+++ escrituracao_contabil.py
@@ -121,7 +121,7 @@ while True:
                 print("O Lançamaneto referente a está operação no Livro Diário ficou da seguinte forma:")
                 print("                                                              ")
                 print("_________________________Livro Diário_________________________")
-                print("| DATA DA OPERAÇÃO: %02d/%02d/%d                              |"%(dia,mes,ano))
+                print("DATA DA OPERAÇÃO: %02d/%02d/%d"%(dia,mes,ano))
                 print("D - %s --- R$ - %d"%(contaD1,valorD1))
                 print("C - %s --- R$ - %d"%(contaC1,valorC1))
                 print("HISTÓRICO: %s"%historico1)
