8356e25c5c1662ff41a6d129e17157314ab541b0
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-03-01 23:15:30 -0300

    Update escrituracao_C_2.0.py

---
+++ escrituracao_C_2.0.py
@@ -0,0 +1,17 @@
+############################################################
+######          ESCRITURAÇÃO CONTÁBIL BÁSICA          ######
+######               OBJETIVO: TREINAR                ######
+######                   DIÊGO AXEL                   ######
+######                    CAICÓ-RM                    ######
+############################################################
+
+# OBS: Estou refazendo...
+
+# imports
+import datetime
+
+data = datetime.date.today()
+
+dia = data.day
+mes = data.month
+ano = data.year
