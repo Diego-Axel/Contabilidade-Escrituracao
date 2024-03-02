b181c66c942f1732e3029684a8c93ede9b0b6774
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-03-02 13:49:20 -0300

    aplicando o pensamento computacional

--- escrituracao_C_2.0.py
+++ escrituracao_C_2.0.py
@@ -5,7 +5,7 @@
 ######                    CAICÓ-RM                    ######
 ############################################################
 
-# OBS: Estou refazendo...
+# OBS: Estou refazendo... Motivos? -> Posso melhorar
 
 # imports
 import datetime
@@ -15,3 +15,27 @@ data = datetime.date.today()
 dia = data.day
 mes = data.month
 ano = data.year
+
+# o que é importante em uma escrituração contábil? o que deve conter, se tratando da básica... ->
+# em ordem seria..
+# se tratando de um sistema completo, seria: 
+
+# INICIO
+# MODOLOS
+#   USUARIO EXISTENTE
+#   NOVO USUAIRO
+#       LOCAL DE CASDATRO
+#           NOME DA EMPRESA/MEI/USUARIO
+#           CPF
+#           CNPJ
+#           ENDEREÇO DE E-MAIL
+#           TEL. PARA CONTATO(DA EMPRESA PREFERENCIALEMNTE) COM DDD
+#           ENDEREÇO DA EMPRESA
+# -------------------------------------------------------      
+# PÓS ESSA PRIMEIRA PARTE  
+# MODULOS
+#   LIVRO DÍARIO
+#   LIVRO RAZÃO
+#   BALANCETE(OPCIONAL)
+#   BALANÇO PATRIMÔNIAL
+# FIM DO PRGRAMA
