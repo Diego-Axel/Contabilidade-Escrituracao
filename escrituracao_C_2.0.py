b86cf32770c7e8ce28bc942d3952c8abb3c196c7
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-09 15:42:17 -0300

    Update escrituracao_C_2.0.py

--- escrituracao_C_2.0.py
+++ escrituracao_C_2.0.py
@@ -181,8 +181,8 @@ def livro_diario_1():
             print("Como havia falado anteriormeente, você você JA POSSUI duas contas 1 DE DÉBITO E 1 DE CRÉDITO.")
             print("") # ESPAÇOS
 
-            tipo_conta = input("Qual o tipo da conta você deseja adicionar(D ou C):? ")
-            if tipo_conta == "Débito" or "Debtio" or "débito" or "debito" or "D" or "d": # CONDICIONAL REFERENTE AO TIPO DA CONTA (D OU C)
+            tipo_conta0 = input("Qual o tipo da conta você deseja adicionar(D ou C):? ")
+            if tipo_conta0 == "Débito" or "Debtio" or "débito" or "debito" or "D" or "d": # CONDICIONAL REFERENTE AO TIPO DA CONTA (D OU C)
                 print("Você escolheu adiconar uma conta ao tipo DÉBITO:")
                 print("")
 
@@ -210,7 +210,7 @@ def livro_diario_1():
                 print("|____________________________________________________________________|")
                 print("") # espaços
             
-            elif tipo_conta == "Crédito" or "Credito" or "crédito" or "credito" or "C" or "c":
+            elif tipo_conta0 == "Crédito" or "Credito" or "crédito" or "credito" or "C" or "c":
                 print("Você escolheu adicionar uma conta aoo tipo CRÉDITO:")
                 print("")
 
@@ -240,10 +240,56 @@ def livro_diario_1():
             
             else:
                 print("Não foi colocado o que se pede.")
-                
 
         elif numero_contas == 2:
-            print("Você escolheu adicionar 2 contas")
+            print("Você escolheu adicionar 2 contas.")
+            print("")
+            print("Como havia falado anteriormeente, você você JA POSSUI duas contas 1 DE DÉBITO E 1 DE CRÉDITO.")
+            print("") # ESPAÇOS
+
+            tipo_conta0 = input("Qual o tipo da 1 CONTA que você deseja adicionar(D ou C):? ")
+            tipo_conta1 = input("Qual o tipo da 2 CONTA que você deseja adicionar(D ou C):? ")
+
+            if tipo_conta0 == ("Débito","débtio","Debito","debito","D","d") and tipo_conta1 == ("Débito","débtio","Debito","debito","D","d"):
+                print("Você escolheu adiconar 2 CONTAS ao Débito.")
+                print("")
+
+                conta_D_main = input("Digite o nome da sua PRIMERIA CONTA de Débito: ")
+                valor_D_main = float(input("Digite o valor(R$) dessa respectiva conta: "))
+                print("__________________________________________________________________")
+
+                conta_D1 = input("Digite o valor da sua SEGUNDA CONTA DE DÉBITO: ")
+                valor_D1 = float(input("Digite o valor(R$) dessa respectiva conta: "))
+                print("__________________________________________________________________")
+
+                conta_D2 = input("Digite o valor da sua TERCEIRA CONTA DE DÉBITO: ")
+                valor_D2 = float(input("Digite o valor(R$) dessa respectiva conta: "))
+                print("__________________________________________________________________")
+
+                conta_C_main = input("Digite o nome da sua PRIMEIRA CONTA de Crédito: ")
+                valor_C_main = float(input("Digite o valor(R$) dessa respectiva conta: "))
+                print("___________________________________________________________________")
+
+                historico1 = input("Digite o histórico referente a está operação: ")
+                print("___________________________________________________________________")
+                print("")
+                print("|____________________________LIVRO_DIÁRIO____________________________|")
+                print("| Data da operação: %02d/%02d/%d"%(dia,mes,ano))
+                print("| D -\t%s\tR$ %.2f"%(conta_D_main,valor_D_main))
+                print("| D -\t%s\tR$ %.2f"%(conta_D1,valor_D1))
+                print("| D -\t%s\tR$ %.2f"%(conta_D2,valor_D2))
+                print("| C -\t%s\tR$ %.2f"%(conta_C_main,valor_C_main))
+                print("| Histórico: %s"%historico1)
+                print("|____________________________________________________________________|")
+                print("") # espaços
+
+
+
+
+
+
+
+
 
 
         else:
