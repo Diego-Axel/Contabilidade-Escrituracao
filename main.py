337011b091ff409c7c731b92f4fc99760fcf7bbb
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-10 23:27:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,22 +40,6 @@ print("") # espaços para evitar ficar colado
 print("Antes de prosseguir, por favor insira qual môdulo você quer escolher: ")
 print("") # espaços
 
-# MODOLOS
-
-# USUARIO EXISTENTE
-def usuario_existente_1():
-    print("|---------------------------------|")
-    print("| Você escolheu Usuário Existente |")
-    print("|---------------------------------|")
-    print("") # espaços
-
-# NOVO USUAIRO
-def novo_usuario_2():
-    print("|---------------------------------|")
-    print("|   Você escolheu Novo Usuário    |")
-    print("|---------------------------------|")
-    print("") 
-
 # começo do While 1:
 while True < 3:
     # solicitar ao usuário que escolha um dos dois môdulos:
@@ -72,54 +56,12 @@ while True < 3:
     print("") 
 
     if escolha == 1:
-        usuario_existente_1()
-        print("Bem vindo(a) de volta!") # quando tiver com os dados e nome da empresa ou do mei fazer com a frase apareça "Bem vindop(a) NOME"
-        print("")
+        funcoes.usuario_existente_1()
         break 
     
     elif escolha == 2:
-        novo_usuario_2()
-        # LOCAL DE CASDATRO
-        print("________________________________________________________________________")
-        print("|                                                                      |")
-        print("|         Vamos seguir para o processo de cadastrar novo usuáriio.     |")
-        print("|                                                                      |")
-        print("|         Por favor, preeencha esses dados para dar continuidade.      |")
-        print("|______________________________________________________________________|")
-        print("")
-        
-        # NOME DA EMPRESA/MEI/USUARIO
-        nome_empresa = input("Digite o nome da sua empresa/ou MEI: ")
-        print("") 
-        # CPF
-        cpf = input("Digite o seu CPF: ")
-        print("")  
-        # CNPJ
-        cnpj = input("Digite o seu cnpj: ")
-        print("")
-        # ENDEREÇO DE E-MAIL
-        email = input("Digite o seu endereço de e-mail: ")
-        print("")
-        # TEL. PARA CONTATO(DA EMPRESA PREFERENCIALEMNTE) COM DDD
-        telefone_empresa = input("Digite o número de telefone(da empresa preferencialmente) com DDD: ")
-        print("")
-        # ENDEREÇO DA EMPRESA
-        endereco_empresa = input("Digite o endereço da empresa: ")
-        print("")
-        print("")
-        print("Seus dados foram cadastrados com sucesso!")
-        print("Dê uma conferida:")
-        print("")
-        # mostrnado os daods que o usuário introduziu no programa:
-        print(" _________________________________________________")
-        print("                                                  ")
-        print("             Nome da empresa/ou MEI: %s           "%nome_empresa)
-        print("                     CPF: %s                      "%cpf)
-        print("                     CNPJ: %s                     "%cnpj)
-        print("                     E-mail: %s                   "%email)
-        print("              Telefone da Empresa: %s             "%telefone_empresa)
-        print("              Endereço da Empresa: %s             "%endereco_empresa)
-        print("__________________________________________________")
+        funcoes.novo_usuario_2()
+       
         break
 
     else:
