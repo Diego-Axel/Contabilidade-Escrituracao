4b5aa6bbfe94a7a45b84be27f6b5e1f701f7e51b
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-16 18:30:44 -0300

    Update funcoes_diario.py

--- funcoes_diario.py
+++ funcoes_diario.py
@@ -46,7 +46,9 @@ def livro_diario_1():
             print("")
             tipo_conta = input("Escolha qual o tipo da próxima conta que deseja adicionar a operção(D/C): ")
 
-            d1 = ["Débito","débito","Debito","debito","D","d"] # Bom, criei uma variável e na mesma adicionei as possiveis strings que o usuário poderá digitar
+            d1 = ["Débito","débito","Debito","debito","D","d"] # Bom, criei uma variável e na mesma adicionei as possiveis strings que o usuário poderá digitar para que de continuidade ao programa. Pate dos D
+
+            c1 = ["Crédito","crédito","Credito","credito","C","c"] # Parte dos C
 
             if tipo_conta == d1:
                 print("Escolha = %s"%tipo_conta)
@@ -59,6 +61,20 @@ def livro_diario_1():
                 print("D\t-\t\t%s\tR$%0.3f"%(conta_d_main,valor_d_main))
                 print("D\t-\t\t%s\tR$%0.3f"%(conta_d1,valor_d1))
                 print("C\t-\t\t%s\tR$%0.3f"%(conta_c_main,valor_c_main))
+                print("Histórico:\t%s"%historico1)
+                print("")
+
+            elif tipo_conta == c1:
+                print("Escolha = %s"%tipo_conta)
+                conta_c1 = input("Digite o nome da sua conta C: ")
+                valor_c1 = float(input("Digite o valor dessa conta: "))
+                print("")
+                historico1 = input("Digite o histórico desta operação: ")
+                print("")
+                print("Data da operação: %02d/%02d/%d"%(dia,mes,ano))
+                print("D\t-\t\t%s\tR$%0.3f"%(conta_d_main,valor_d_main))
+                print("C\t-\t\t%s\tR$%0.3f"%(conta_c_main,valor_c_main))
+                print("C\t-\t\t%s\tR$%0.3f"%(conta_c1,valor_c1))
                 print("Histórico:\t%s"%historico1)
                 print("")
 
