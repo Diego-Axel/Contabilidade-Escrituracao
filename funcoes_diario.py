abee05a6f78c8ca265f0044d5985b49306856c0a
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-26 19:55:13 -0300

    Update funcoes_diario.py

--- funcoes_diario.py
+++ funcoes_diario.py
@@ -75,7 +75,7 @@ def livro_diario():
 
             c1 = ["Crédito","crédito","Credito","credito","C","c"] # Parte dos Créditos
 
-            if tipo_conta in d1:
+            if (tipo_conta in d1):
                 tipo_conta = tipo_conta.upper()
                 print("Escolha = %s"%tipo_conta)
                 conta_d1 = input("Digite o nome da sua conta D: ")
