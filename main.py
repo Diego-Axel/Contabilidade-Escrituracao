4200bbbaa6b3bd80624d1afcdff5fff558960b68
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-29 00:08:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -91,18 +91,17 @@ print("")
 time.sleep(8)
 
 print("Eis o modelo de cada um dos passos apresentados:")
+
 print("""
 
-
-
-
-
-
-
-
-
-
-
+    |         LIVRO DIÁRIO          |
+      
+    data da operação: xx/xx/xxxx
+    D - xxxx - R$ xx.xxx,xx
+    C - xxxx - R$ xx.xxx,xx
+    histórico: xxxxxxxxxxxx
+    (Pode ter mais de uma conta de débito e crédito.)
+    
     """)
 
 os.system('clear || cls')
