e60bfd76b3a48a3f0bdb1da479de1df3f431aab4
Diego-Axel <diegoaxelbsr@gmail.com>
2024-04-15 20:31:44 -0300

    Update exemplos_modulos.py

---
+++ exemplos_modulos.py
@@ -0,0 +1,12 @@
+def exemplo_livro_diario():
+    print("""
+      
+            |         LIVRO DIÁRIO          |
+      
+      data da operação: xx/xx/xxxx
+      D - xxxx - R$ xx.xxx,xx
+      C - xxxx - R$ xx.xxx,xx
+      histórico: xxxxxxxxxxxx
+      (Pode ter mais de uma conta de débito e crédito.)
+      
+    """)
