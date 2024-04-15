83493e50f378ae72d24fede8b81d158a7858a448
Diego-Axel <diegoaxelbsr@gmail.com>
2024-04-15 20:35:29 -0300

    Update exemplos_modulos.py

--- exemplos_modulos.py
+++ exemplos_modulos.py
@@ -47,3 +47,24 @@ def exemplo_balancete():
       
     """)
 
+def exemplo_balanco_patrimonial():
+    print("""
+
+            |       BALANÇO PATRIMÔNIAL       |
+    ____________________________________________________
+    |        ATIVOS         |         PASSIVOS         |
+    |_______________________|__________________________|
+    |  Caixa R$ xx.xxx,xx   |  Fornecedor R$ xx.xxx,xx |
+    |  Banco R$ xx.xxx,xx   | Conta/pagar R$ xx.xxx,xx |
+    | Estoques R$ xx.xxx,xx |                          |
+    | Veículos R$ xx.xxx,xx |                          |
+    |_______________________|__________________________|
+    |                       |    PATRIMÔNIO LÍQUIDO    |
+    |_______________________|__________________________|
+    |                       | Cap.Social  R$ xx.xxx,xx |
+    |                       |                          |
+    |_______________________|__________________________|
+    |___TOTAL ATIVOS (R$)___|___TOTAL PASSIVOS + PL____|
+    |_______________________|__________________________|
+      
+    """)
