88d63716249333fd660de0666716e56ac1d19a25
Diego-Axel <diegoaxelbsr@gmail.com>
2024-04-26 12:49:25 -0300

    recomeço do livro diário
    deixando mais limpo e comentado

--- livro_diario.py
+++ livro_diario.py
@@ -2,10 +2,9 @@
 
 import main
 import ascii
-import datetime # biblioteca para puxar a data do computador
-import os # biblioteca para interagir com o sistema operacional, estou usando para que tudo que esteja acima dela, seja removido. fica mais "limpo" de se vizualizar
-import time # da um tempo para as informações sumirem -> time.sleep(o segundo que você quiser aqui quendo dos parenteses)
-
+import datetime 
+import os 
+import time 
 
 data = datetime.date.today()
 
@@ -13,18 +12,51 @@ dia = data.day
 mes = data.month
 ano = data.year
 
-
-os.system('clear || cls')
+os.system('clear || cls') # Se for Linux use "clear" e se for Windowns use "cls"
 ascii.livro_diario_ascii()
-
-print("CARREGANDO...")
 print()
-time.sleep(4)
-os.system('clear || cls')
 
 # PEDIR O NÚMERO DE OPERAÇÕES
-numero_operacoes = int(input("Digite quantas operações você deseja realizar (1,2,3,4...): "))
+num_op = input("Digite quantas operações você deseja realizar (1,2,3,4...): ")
+print()
+
+if num_op == "1":
+    os.system('clear || cls') # Se for Linux use "clear" e se for Windowns use "cls"
+    print("1 OPERAÇÃO:")
+    num_contas = input("Quantas contas terá nesta operção (POR PADRÃO JA TEM 1 DE DÉBITO E 1 DE CRÉDITO, ESCOLHA 0 PARA NÃO ADICIONAR)? ")
+    print()
+    if num_contas == "0":
+        valor_d0 = 0
+        valor_c0 = 0
+        while valor_d0 == valor_c0:
+            os.system('clear || cls')
+            print("NENHUMA CONTA ADICIONADA 1 - DÉBITO e 1 - CRÉDTIO (PADRÃO).")
+            print()
+            conta_d0 = input("Digite o nome da sua 1ª conta de DÉBITO: ")
+            valor_d0 = float(input("Digite o valor R$ da conta %s: "%conta_d0))
+            print()
+            conta_c0 = input("Digite o nome da sua 1ª conta de CRÉDITO: ")
+            valor_c0 = float(input("Digite o valor R$ da conta %s: "%conta_c0))       
+            if valor_d0 != valor_c0:
+                print("OPS! Os valores das contas não estão batendo com igualdade, por favor insira-os novamente.")
+                print()
+                input("tecle <ENTER> para retornar a inserir os dados...")
+                valor_d0 = 0
+                valor_c0 = 0
+            else:
+                historico0 = input("Digite o histórico referente a está operação: ")
+            
+
+
     
+    
+
+
+
+
+
+
+
 
 
 
