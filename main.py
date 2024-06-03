4a22060ae1d7c47b5bd402f44b9a6fdeaa41427e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-03 16:33:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,19 @@
 ######                   DIÊGO AXEL                   ######
 ######                    CAICÓ-RM                    ######
 ############################################################
+
+'''imports'''
+import os
+import datetime
+import diario
+
+'''datetime'''
+data = datetime.date.today()
+dia = data.day
+mes = data.month
+ano = data.year
+
+'''código principal'''
+data = float(input("Digite o valor de D: "))
+
+dr = diario.Livro_Diario(data)
