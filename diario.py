0c5c5914be68218a6c8d9e47f95d010b74972334
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-05 16:54:40 -0300

    Delete diario.py

--- diario.py
+++
@@ -1,9 +0,0 @@
-class Livro_Diario:
-    def __init__(self, data, conta_d, valor_cd, conta_c, valor_cc, historico):
-        self.data = data
-        self.conta_d = conta_d
-        self.valor_cd = valor_cd
-        self.conta_c = conta_c
-        self.valor_cc = valor_cc
-        self.historico = historico
-        print("DATA OK")
