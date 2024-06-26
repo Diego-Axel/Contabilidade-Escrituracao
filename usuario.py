be257fad0043ed584fb106acc09d00c31489f34e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-26 00:12:55 -0300

    refatorado

--- usuario.py
+++ usuario.py
@@ -1,11 +1,9 @@
-'''Banco de Dados (PRIVATE)'''
-
 '''imports'''
 import psycopg2 
-import cr_query as create
-import insrt_query as insert
-import sel_query as select
+import banco_usuario.us_create_query as create
+import banco_usuario.us_insert_query as insert
 import os
+import time
 
 # Conectar ao banco de dados --> PostgreSQL
 
@@ -19,13 +17,10 @@ def usuarios():
             database="escrituracao"
         )
         cursor = connection.cursor()
-
         # Criar Tabela
         create.create_table_us()
-
         cursor.execute(create.create_table_us())
         connection.commit()
-
         # Inserir dados na tabela
         os.system("clear || cls")
         print("------ REGISTRO PARA FEEDBACK ------")
@@ -40,139 +35,19 @@ def usuarios():
         print()
         print("Dados salvos com sucesso!")
         input("telce <ENTER> para continuar...")
-
         # INSERT INTO
         insert.insert_into_us()
-
         cursor.execute(insert.insert_into_us(), (nome, sobrenome, satisfacao, melhoria))
         id_inserido = cursor.fetchone()[0]
         connection.commit()
+        print()
         print(f"Usuário inserido com ID: {id_inserido}")
-                
+        time.sleep(5)           
     except (Exception, psycopg2.Error) as error:
         print("Erro ao conectar ou operar no PostgreSQL", error)
-
     finally:
         # Fechar conexão
         if connection:
             cursor.close()
             connection.close()
             print("Conexão com PostgreSQL fechada")
-
-
-def banco_debito():
-    try:
-        connection = psycopg2.connect(
-            user="postgres",
-            password="palmeiras123",
-            host="localhost",
-            port="5432",
-            database="escrituracao"
-        )
-        cursor = connection.cursor()
-        
-        # Criar tabela
-        create_table_query = '''
-        CREATE TABLE IF NOT EXISTS debito (
-            id SERIAL PRIMARY KEY,
-            nome VARCHAR(30),
-            valor NUMERIC
-        );
-        '''
-        cursor.execute(create_table_query)
-        connection.commit()
-
-        # Inserir dados na tabela
-        nome = input("Digite o nome da sua conta de Débito: ")
-        valor = input("Digite o valor dessa conta: ")
-        
-        insert_query = '''
-        INSERT INTO debito (nome, valor)
-        VALUES (%s, %s)
-        RETURNING id;
-        '''
-        cursor.execute(insert_query, (nome, valor))
-        id_inserido = cursor.fetchone()[0]
-        connection.commit()
-        print(f"Dado inserido com ID: {id_inserido}")
-
-        # Consultar os dados inseridos
-        # select_query = "SELECT * FROM debito;"
-        # cursor.execute(select_query)
-        # records = cursor.fetchall()
-        
-        # print("Dados na tabela 'debito':")
-        # for row in records:
-        #     print(f"ID: {row[0]}, Nome: {row[1]}, Valor: {row[2]}")
-            
-
-    except (Exception, psycopg2.Error) as error:
-        print("Erro ao conectar ou operar no PostgreSQL", error)
-
-    finally:
-        # Fechar conexão
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Conexão com PostgreSQL fechada")
-
-
-def banco_credito():
-    try:
-        connection = psycopg2.connect(
-            user="postgres",
-            password="palmeiras123",
-            host="localhost",
-            port="5432",
-            database="escrituracao"
-        )
-        cursor = connection.cursor()
-        
-        # Criar tabela
-        create_table_query = '''
-        CREATE TABLE IF NOT EXISTS credito (
-            id SERIAL PRIMARY KEY,
-            nome VARCHAR(30),
-            valor NUMERIC
-        );
-        '''
-        cursor.execute(create_table_query)
-        connection.commit()
-
-        # Inserir dados na tabela
-        nome = input("Digite o nome da sua conta de Crédtio: ")
-        valor = input("Digite o valor dessa conta: ")
-        
-        insert_query = '''
-        INSERT INTO credito (nome, valor)
-        VALUES (%s, %s)
-        RETURNING id;
-        '''
-        cursor.execute(insert_query, (nome, valor))
-        id_inserido = cursor.fetchone()[0]
-        connection.commit()
-        print(f"Dado inserido com ID: {id_inserido}")
-
-        # Consultar os dados inseridos
-        # select_query = "SELECT * FROM credito;"
-        # cursor.execute(select_query)
-        # records = cursor.fetchall()
-        
-        # print("Dados na tabela 'credito':")
-        # for row in records:
-        #     print(f"ID: {row[0]}, Nome: {row[1]}, Valor: {row[2]}")
-            
-
-    except (Exception, psycopg2.Error) as error:
-        print("Erro ao conectar ou operar no PostgreSQL", error)
-
-    finally:
-        # Fechar conexão
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Conexão com PostgreSQL fechada")
-
-
-def banco_historico():
-    print()
